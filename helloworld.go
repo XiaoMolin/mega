25fead68087864b0306b34a2428972ebcf0634ea
unknown <a1320861682@outlook.com>
2020-12-20 11:18:02 +0800

    a

---
+++ helloworld.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("helloworld"))
+	})
+	http.ListenAndServe(":8000", nil)
+}
