c3c124ad6b3e59a62c6c24e66cec0d88f7c72f9d
unknown <a1320861682@outlook.com>
2020-12-20 20:09:10 +0800

    temolatebasic

--- TemplateBasic/main.go
+++ TemplateBasic/main.go
@@ -14,12 +14,18 @@ func main() {
 	cwd, _ := os.Getwd()
 	fmt.Println(filepath.Join(cwd, "mega/TemplateBasic/templates/index.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		u1, u2 := model.User{Username: "lxm"}, model.User{Username: "xml"}
+		posts := []model.Post{
+			{User: u1, Body: "aaa"},
+			{User: u2, Body: "aaa"},
+		}
 		user := model.User{Username: "lxm"}
+		v := model.IndexViewModel{Title: "Homepage", User: user, Posts: posts}
 		tpl, _ := template.ParseFiles("src/mega/TemplateBasic/templates/index.html")
-		err := tpl.Execute(w, &user)
+		err := tpl.Execute(w, &v)
 		if err != nil {
 			log.Fatal(err)
 		}
 	})
-	http.ListenAndServe(":8888", nil)
+	_ = http.ListenAndServe(":8888", nil)
 }
--- TemplateBasic/model/model.go
+++ TemplateBasic/model/model.go
@@ -3,3 +3,14 @@ package model
 type User struct {
 	Username string
 }
+
+type IndexViewModel struct {
+	Title string
+	User  User
+	Posts []Post
+}
+
+type Post struct {
+	User User
+	Body string
+}
