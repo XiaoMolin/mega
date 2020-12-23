7320cfb708a41a0477ce5cd90641459d1e1d2b24
unknown <a1320861682@outlook.com>
2020-12-23 14:40:33 +0800

    4.0

--- vm/index.go
+++ vm/index.go
@@ -7,16 +7,22 @@ import (
 type IndexViewModel struct {
 	BaseViewModel
 	Posts []model.Post
+	Flash string
 }
 
 // IndexViewModelOp struct
 type IndexViewModelOp struct{}
 
 // GetVM func
-func (IndexViewModelOp) GetVM(username string) IndexViewModel {
+func (IndexViewModelOp) GetVM(username string,flash string) IndexViewModel {
 	u1, _ := model.GetUserByUsername(username)
 	posts, _ := model.GetPostsByUserID(u1.ID)
-	v := IndexViewModel{BaseViewModel{Title: "Homepage"}, *posts}
+	v := IndexViewModel{BaseViewModel{Title: "Homepage"}, *posts,flash}
 	v.SetCurrentUser(username)
 	return v
+}
+
+func CreatePost(username,post string)error{
+	u,_:=model.GetUserByUsername(username)
+	return u.CreatePost(post)
 }
